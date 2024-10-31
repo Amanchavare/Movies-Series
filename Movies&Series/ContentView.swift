430a2ef3a7a85aebacd82fd9c314be518a3bae05
Amanchavare <amanchavare2002@gmail.com>
2024-10-31 15:02:20 +0100

    Initial Commit

---
+++ Movies&Series/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movies&Series
+//
+//  Created by Aman Niranjan Chavare on 31/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movies&Series/Movies_SeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movies_SeriesApp.swift
+//  Movies&Series
+//
+//  Created by Aman Niranjan Chavare on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Movies_SeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
